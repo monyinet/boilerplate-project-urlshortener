fe9342ecc7c1e4d346a83a4158a60939a60b6ddf
Jose Juan Moñino Coll <holamundo@monyi.dev>
2022-11-15 19:40:39 +0100

    add dns.lookup

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const bodyParser = require('body-parser');
 const app = express();
 
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+function isValidUrl(url) {
+  return /^(http|https):\/\/[^ "]+$/.test(url);
+}
 
 app.use(cors());
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -22,11 +28,23 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', urlencodedParser, (req, res) => {
-  console.log(req.body);
-  res.json({ original_url: req.body.url, short_url: 1 });
-});
-  
+app.post('/api/shorturl', urlencodedParser, (req, res, next) => {
+  // console.log(req.body);
+  const url = req.body.url;
+  const checkUrl = isValidUrl(url);
+  console.log(checkUrl);
+  if (checkUrl) {
+    try {
+      dns.lookup(url, (err, address, family) => {
+        err == null ? res.json({ error: 'invalid URL' }) : res.json({ original_url: url, short_url: 1 });         
+      });
+    } catch (error) {
+      res.json({ error: 'invalid url' });
+    }
+  } else {
+    res.json({ error: 'invalid url' });
+  }  
+});  
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
