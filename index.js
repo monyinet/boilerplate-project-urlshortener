c62940c93034e2db13ae682ebc1f90664305316f
Jose Juan Moñino Coll <36202313+monyinet@users.noreply.github.com>
2022-11-27 01:24:31 +0000

    add lowdb package and convert to modules

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const dns = require('dns');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+import { config } from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import dns from 'dns';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 const app = express();
 
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
+// File path
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'db.json')
+
+// Configure lowdb to write to JSONFile
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+
+// Read data from JSON file, this will set db.data content
+await db.read()
+
+// If db.json doesn't exist, db.data will be null
+// Use the code below to set default data
+// db.data = db.data || { posts: [] } // For Node < v15.x
+db.data ||= { posts: [] }             // For Node >= 15.x
+
+// Create and query items using native JS API
+db.data.posts.push('hello world')
+const firstPost = db.data.posts[0]
+
+// Alternatively, you can also use this syntax if you prefer
+const { posts } = db.data
+posts.push('hello world')
+
+// Finally write db.data content to file
+await db.write()
 
 
 // Basic Configuration
@@ -36,9 +68,9 @@ app.post('/api/shorturl', urlencodedParser, (req, res, next) => {
   const url = req.body.url;
   const checkUrl = isValidUrl(url);
   console.log(checkUrl);
-  if (checkUrl) {
+  if (checkUrl) {		
     try {
-      dns.lookup(url, (err, address, family) => {
+      dns.lookup(url, (err, address, family) => {				
         err == null ? res.json({ error: 'invalid URL' }) : res.json({ original_url: url, short_url: 1 });         
       });
     } catch (error) {
@@ -48,6 +80,14 @@ app.post('/api/shorturl', urlencodedParser, (req, res, next) => {
     res.json({ error: 'invalid url' });
   }  
 });  
+
+app.get('/api/shorturl/:id', (req, res) => {
+	let id = req.params.id;
+	if (id == 1) {
+		res.redirect('freecodecamp.org')
+	}
+	console.log(id);
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
